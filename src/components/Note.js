2a3d9573116ca2c5ebe431aa3b06f503c8c24605
Chris Barber-Riley <chris.barberriley@gmail.com>
2019-10-13 20:35:36 +0100

    Added styles and animation to the list

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,18 +1,24 @@
 import React, { useContext } from "react";
 import NotesContext from "../context/NotesContext";
+import { useSpring, animated } from "react-spring";
 
 const Note = ({ note: { title, message } }) => {
+    const props = useSpring({ opacity: 1, from: { opacity: 0 } });
     const { dispatch } = useContext(NotesContext);
     return (
-        <div
+        <animated.div
+            style={props}
+            className="todo"
             key={title}
             onClick={() => dispatch({ type: "REMOVE_NOTE", title })}
         >
-            <br />
-            Title: {title}
-            <br />
-            Message: {message}
-        </div>
+            <p>
+                <span className="title">Title: </span> {title}
+            </p>
+            <p>
+                <span className="title">Message: </span> {message}
+            </p>
+        </animated.div>
     );
 };
 
