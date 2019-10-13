cd34c8fb534fbe249ac6c3d82d2f02a2cdad7603
Chris Barber-Riley <chris.barberriley@gmail.com>
2019-10-13 15:47:26 +0100

    Reducer setup and reading from local storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useReducer } from "react";
+import notesReducer from "./reducers/notes-reducer";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [title, setTitle] = useState("");
+    const [message, setMessage] = useState("");
+    // const [notes, setNotes] = useState([]);
+    const [notes, dispatch] = useReducer(notesReducer, []);
 
-export default App;
+    useEffect(() => {
+        const notes = JSON.parse(localStorage.getItem("notes"));
+
+        dispatch({ type: "POPULATE_NOTES", notes });
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+
+    const addNotes = e => {
+        e.preventDefault();
+        dispatch({ type: "ADD_NOTE", title, message });
+        setTitle("");
+        setMessage("");
+    };
+
+    const removeNote = title => {
+        dispatch({ type: "REMOVE_NOTE", title });
+        // setNotes(notes.filter(note => note.title !== title));
+    };
+    return (
+        <div className="App">
+            <form onSubmit={addNotes}>
+                <input
+                    type="text"
+                    placeholder="Title"
+                    onChange={e => setTitle(e.target.value)}
+                    value={title}
+                />
+                <input
+                    type="text"
+                    placeholder="Messgae"
+                    onChange={e => setMessage(e.target.value)}
+                    value={message}
+                />
+                <input type="submit" />
+            </form>
+            {notes.map(note => (
+                <div key={note.title} onClick={() => removeNote(note.title)}>
+                    <br />
+                    Title: {note.title}
+                    <br />
+                    Message: {note.message}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export { App as default };
